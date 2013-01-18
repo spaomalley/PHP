6581a9e203e2d16bcf1f40c6798f565f5d64bac2
spaomalley <Sean.OMalley@pps.io>
2013-01-18 13:39:07 -0500

    Update postOrder.php
    ***Uses MultToke.php.   Creates an order and echos the orderId from the Location header

--- postOrder.php
+++ postOrder.php
@@ -1,104 +1,91 @@
 <?php
+
 require 'multToke.php';
 
-function urlencode_oauth($str) {
-  return
-    str_replace('+',' ',str_replace('%7E','~',rawurlencode($str)));
-}
+function createOrder($consumerSecret, $link, $method,$token,$tokenSecret,$params,$merchId,$custId,$custAlias){
+    $params['oauth_token']=$token;
 
+     $queryParts=array(
+        'merchantId'=>'-'.$merchId 
+     );   
+    $queryString=array();
+ 
+    foreach ($queryParts as $key=>$value) {
+        $queryString[] = urlencode_oauth($key).'='.urlencode_oauth($value);
+    }
+    $queryString = implode('&',$queryString); 
 
-$url='https://test.api.mxmerchant.com/v1/order';
-$params = array(
-    'merchantId'=>'',  // <<<<<<<The Merchant Id
-  'oauth_token'=>$t,
-  'oauth_consumer_key'=>$ccKey,
-  'oauth_nonce'=>sha1(microtime()),
-  'oauth_signature_method'=>'HMAC-SHA1',
-  'oauth_timestamp'=> time(),  
-  'oauth_version'=>'1.0'
-);
+    $newArray=  array_merge($queryParts,$params);    
+    ksort($newArray);
 
-// sort parameters according to ascending order of key
-ksort($params);
-
-// prepare URL-encoded query string
-$q = array();
-foreach ($params as $key=>$value) {
-  $q[] = urlencode_oauth($key).'='.urlencode_oauth($value);
-}
-$q = implode('&',$q);
-
-
-
-$parts = array(
-  'POST',
-  urlencode_oauth($url),
-  urlencode_oauth($q)
-);
-//Concatenate with &
-$base_string = implode('&',$parts);
-
-
-$key = urlencode_oauth($ccSecret) . '&'.  urlencode_oauth($ts);
-
-
-$signature = base64_encode(hash_hmac('sha1',$base_string,$key,true));
-
-//Append the signature to your array
-$params['oauth_signature'] = $signature;
-$str = array();
-//Re-URL encode all your values including the signature this time
-foreach ($params as $k=>$value) {
-  $str[] = $k . '="'.urlencode_oauth($value).'"';
-}
-
-
-$str = implode(',',$str);
-
-//Create Authorization Header
-$headers = array(
-  'Authorization: OAuth '.$str,
-  'Content-Type: application/json'
-  //'Content-Length: 0',
-  //'Connection: close'
-);
-
-
-
-$info = json_encode(array(
-
-"merchantId"=>'',//<<<<<The Merchant Id
-"type"=>"Sale",
-"quantity"=>1,
-"totalAmount"=>"3",
-"taxAmount"=>"0",
-"subTotalAmount"=>"3",
-"tipAmount"=>"0",
- "discountAmount"=>"0",
-"balance"=>"90",
-  
-"purchases"=> array("productName"=>"test1",
-                    "quantity"=>1,
-                    "price"=>"3",
-                    "discountAmount"=>"0",
-                    "subTotalAmount"=>"3",
-                    "taxRate"=>"0",
-                    "taxAmount"=>"0",
-                    "totalAmount"=>"3",
-                    ) ,
-"discounts"=>array(),
-"customer"=>array(
-"id"=>"",//<<<<<<the Id of the customer you are attaching the order to.  ie:31fbm8l9c-e006-4145-8fd4-97679ee5fc08
-"alias"=>"Craig"
-)
+    $q = array();
+    foreach ($newArray as $key=>$value) {
+        $q[] = urlencode_oauth($key).'='.urlencode_oauth($value);
+    }
+    $q = implode('&',$q);
     
-));
+    $parts = array(
+        $method,
+        urlencode_oauth($link),
+        urlencode_oauth($q)
+    );
+    $base_string = implode('&',$parts);
+
+
+    $key = urlencode_oauth($consumerSecret) . '&'.  urlencode_oauth($tokenSecret);
+
+    $signature = base64_encode(hash_hmac('sha1',$base_string,$key,true));
+
+    $newArray['oauth_signature'] = $signature;
+    $str = array();
+    foreach ($newArray as $k=>$value) {
+        $str[] = $k . '="'.urlencode_oauth($value).'"';
+    }
+
+
+    $str = implode(',',$str);
+
+    $headers = array(
+        'Authorization: OAuth '.$str,
+    'Content-Type: application/json'
+    );
+
+
+
+    $info = array(
+
+        "merchantId"=>$merchId,
+        "type"=>"Sale",
+        "quantity"=>1,
+        "totalAmount"=>"3",
+        "taxAmount"=>"0",
+        "subTotalAmount"=>"3",
+        "tipAmount"=>"0",
+        "discountAmount"=>"0",
+        "balance"=>"90",
+  
+        "purchases"=> array(array("productName"=>"test1",
+                            "quantity"=>"1",
+                            "price"=>"3",
+                            "discountAmount"=>"0",
+                            "subTotalAmount"=>"3",
+                            "taxRate"=>"0",
+                            "taxAmount"=>"0",
+                            "totalAmount"=>"3",
+                            )) ,
+        "discounts"=>array(),
+        "customer"=>array(
+        "id"=>$custId,
+        "alias"=>$custAlias
+        )
+    
+    );
 
 
     $options = array(CURLOPT_HTTPHEADER => $headers, //use our authorization 
-                           CURLOPT_URL => $url.'?'."",// <<<<<<<The Merchant ID
+                           CURLOPT_URL => $link.'?'.$queryString,
                            CURLOPT_POST => true,
-                           CURLOPT_POSTFIELDS => $info ,//this is going to be a POST - required   
+                           CURLOPT_POSTFIELDS => json_encode($info) ,//this is going to be a POST - required   
                            CURLOPT_RETURNTRANSFER => true, //return content as a string, don't echo out directly
                            CURLOPT_SSL_VERIFYPEER => false //don't verify SSL certificate, just do it
                     ); 
@@ -108,8 +95,18 @@ $info = json_encode(array(
     curl_close($ch); //hang up
     echo $response;
   
-    //The Location response header contains the  ID of the created Order/invoice
-    //This can be seen in firebug/Fiddler
-   
-
-?>
+    
+}
+    //Fill in the following three variables with the
+    //merchantId
+    //customer id
+    //customer alias(name)
+    $merchId;
+    $custId;
+    $custAlias;
+    $order=createOrder($consumerSecret, $links['createOrder'][0], $links['createOrder'][1],$token,$tokenSecret,$params,$merchId,$custId,$custAlias);
+    $loc=$order['Location'];
+    $pos =strrpos($loc, "/");
+    $orderId = substr($loc,$pos+1); 
+    echo "This is the id of your order. ".$orderId."<br />  You can submit this back to GET /order/{id} to see the information you just submitted."
+         ?>
